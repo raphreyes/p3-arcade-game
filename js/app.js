237443b7d0f87486ca1dbb71485cb07d9c92a954
raphreyes <schmexy@gmail.com>
2015-12-12 18:07:44 -0800

    Add collision detection and win

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 // Enemies our player must avoid
 var Enemy = function(laneNumber) {
-    // Variables applied to each of our instances go here
-	// Make a fast or slow enemy
-
 	// Start enemies at left completely off the game field
 	// Start enemy up to 4 widths off-screen
 	this.x = (Math.random() * 404) * -1;
-	// Assign the lane and speed
+
+	// Assign the enemy lane and speed
 	this.lane = laneNumber;
 	if(this.lane == 1) {
 		this.y = 228;
@@ -20,8 +18,6 @@ var Enemy = function(laneNumber) {
 			this.speed = 3;
 		}
 	}
-	console.log(this.y);
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -36,7 +32,7 @@ Enemy.prototype.update = function(dt) {
 	// if the enemy moves off right edge,
 	// move them back to left side
 	if(this.x > 505) {
-		// Move the enemy back beyond left edge of board
+		// Move the enemy random distance beyond left edge of board
 		this.x = (Math.random() * 404) * -1;
 	}
 };
@@ -59,57 +55,92 @@ var Player = function() {
 Player.prototype.update = function() {
 	// Check for goal
 	if (this.y == -10) {
-		this.score = this.score + 10;
-		this.y = 400;
-
+		this.reset();
 	}
 
 	// check lane1 enemy
-	if (this.y == 236 && this.x < enemy3.x + 40 && this.x > enemy3.x - 40) {
-		// this.tradgedy();
+	if (this.y == 236) {
+		if (enemy1.x >= this.x - 20 && enemy1.x <= this.x + 20) {
+			this.tradgedy();
+		}
 	}
-
-	if (this.y == 154 && this.x < enemy2.x + 40 && this.x > enemy3.x - 40) {
-		//tradgedy
+	// check lane2 enemy
+	if (this.y == 154) {
+		if (enemy2.x >= this.x - 20 && enemy2.x <= this.x + 20) {
+			this.tradgedy();
+		}
 	}
-
-	if (this.y == 72 && this.x < enemy1.x + 40 && this.x > enemy3.x - 40) {
-		//tradgedy
+	//check lane3 enemy
+	if (this.y == 72) {
+		if (enemy3.x >= this.x - 20 && enemy3.x <= this.x + 20) {
+			this.tradgedy();
+		}
 	}
 	// console.log(this.score);
 }
 
 Player.prototype.handleInput = function(input) {
-
+	this.condition = "normal";
 	// move player up unless they are at the top
 	if (input == "up" && this.y > -10) {
 		this.y = this.y - 82;
 	}
-
 	// move player down unless they are at the bottom
 	if (input == "down" && this.y < 400) {
 		this.y = this.y + 82;
 	}
-
 	// move player left unless they are at the left edge
 	if (input == "left" && this.x > -2) {
 		this.x = this.x - 101;
 	}
-
 	// move player right unless they are at the right edge
 	if (input == "right" && this.x < 402) {
 		this.x = this.x + 101;
 	}
-
-	player.update()
+	// console.log("player:" + this.x, this.y, this.condition);
 }
 
 Player.prototype.render = function() {
+	// if player collides with enemy
+	if (this.condition == "ouch") {
+			this.sprite = "images/char-boy-r45.png";
+	}
+
+	if (this.condition == "win") {
+			this.sprite = "images/char-boy-gold.png";
+	}
+
+	if (this.condition == "normal") {
+		this.sprite = "images/char-boy.png";
+	}
+
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	console.log(this.score);
+	ctx.fillText = (this.score, 10, 10);
+
 }
 
 Player.prototype.tradgedy = function() {
+	this.condition = "ouch";
+	var penalty =  player.y + 82;
+	setTimeout(function(){
+		player.y = player.y + 6;
+		console.log(player.y);
+		player.condition = "normal";
+		player.y =  penalty;
+	}, 250)
+	player.score =  player.score - 1;
+}
 
+
+// Player reaches top, reset to botom
+Player.prototype.reset = function() {
+	this.condition = "win";
+	setTimeout(function(){
+		player.y = 400;
+		player.condition = "normal"
+	}, 1000)
+	player.score = player.score + .5;
 }
 
 // Now instantiate your objects.
--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+		'images/char-boy-l45.png',
+		'images/char-boy-r45.png',
+		'images/char-boy-gold.png'
     ]);
     Resources.onReady(init);
 
